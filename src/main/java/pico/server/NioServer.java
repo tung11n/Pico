3f5b16f2c0357a2cc9bc0e0db49f533336a9fd2c
Tung Tran <tungtt@gmail.com>
2014-10-26 21:10:40 -0400

    Add pico service

---
+++ src/main/java/pico/server/NioServer.java
@@ -0,0 +1,142 @@
+package pico.server;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
+
+/**
+ * Created by tungtt on 12/20/13.
+ */
+public class NioServer implements Runnable {
+    // The host:port combination to listen on
+    private InetAddress hostAddress;
+    private int port;
+
+    // The channel on which we'll accept connections
+    private ServerSocketChannel serverChannel;
+
+    // The selector we'll be monitoring
+    private Selector selector;
+
+    // The buffer into which we'll read data when it's available
+    private ByteBuffer readBuffer = ByteBuffer.allocate(8192);
+
+    private EchoWorker worker;
+
+    public NioServer(InetAddress hostAddress, int port, EchoWorker worker) throws IOException {
+        this.hostAddress = hostAddress;
+        this.port = port;
+        this.selector = this.initSelector();
+        this.worker = worker;
+    }
+
+    public static void main(String[] args) {
+        try {
+            EchoWorker worker = new EchoWorker();
+            new Thread(worker).start();
+            new Thread(new NioServer(null, 9090, worker)).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                // Wait for an event one of the registered channels
+                this.selector.select();
+
+                // Iterate over the set of keys for which events are available
+                Iterator selectedKeys = this.selector.selectedKeys().iterator();
+                while (selectedKeys.hasNext()) {
+                    SelectionKey key = (SelectionKey) selectedKeys.next();
+                    selectedKeys.remove();
+
+                    if (!key.isValid()) {
+                        continue;
+                    }
+
+                    // Check what event is available and deal with it
+                    if (key.isAcceptable()) {
+                        this.accept(key);
+                    } else if (key.isReadable()) {
+                        this.read(key);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Selector initSelector() throws IOException {
+        // Create a new selector
+        Selector socketSelector = SelectorProvider.provider().openSelector();
+
+        // Create a new non-blocking server socket channel
+        this.serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+
+        // Bind the server socket to the specified address and port
+        InetSocketAddress isa = new InetSocketAddress(this.hostAddress, this.port);
+        serverChannel.socket().bind(isa);
+
+        // Register the server socket channel, indicating an interest in
+        // accepting new connections
+        serverChannel.register(socketSelector, SelectionKey.OP_ACCEPT);
+
+        return socketSelector;
+    }
+
+    private void accept(SelectionKey key) throws IOException {
+        // For an accept to be pending the channel must be a server socket channel.
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+
+        // Accept the connection and make it non-blocking
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        Socket socket = socketChannel.socket();
+        socketChannel.configureBlocking(false);
+
+        // Register the new SocketChannel with our Selector, indicating
+        // we'd like to be notified when there's data waiting to be read
+        socketChannel.register(this.selector, SelectionKey.OP_READ);
+    }
+
+    private void read(SelectionKey key) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+
+        // Clear out our read buffer so it's ready for new data
+        this.readBuffer.clear();
+
+        // Attempt to read off the channel
+        int numRead;
+        try {
+            numRead = socketChannel.read(this.readBuffer);
+        } catch (IOException e) {
+            // The remote forcibly closed the connection, cancel
+            // the selection key and close the channel.
+            key.cancel();
+            socketChannel.close();
+            return;
+        }
+
+        if (numRead == -1) {
+            // Remote entity shut the socket down cleanly. Do the
+            // same from our end and cancel the channel.
+            key.channel().close();
+            key.cancel();
+            return;
+        }
+
+        // Hand the data off to our worker thread
+        this.worker.processData(this, socketChannel, this.readBuffer.array(), numRead);
+    }
+}
---
+++ src/main/java/pico/server/PicoServer.java
@@ -0,0 +1,11 @@
+package pico.server;
+
+/**
+ * Created by tungtt on 1/12/14.
+ */
+public class PicoServer {
+
+    public void start() {
+
+    }
+}
